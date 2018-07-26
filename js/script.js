2699ab8b5cd45acc366f1af31618c9f2151f64f4
alias-3 <wanianurag3@gmail.com>
2018-07-26 18:54:32 +0530

    buttons added

--- js/script.js
+++ js/script.js
@@ -19,18 +19,25 @@ function search(e) {
     $.getJSON("https://www.googleapis.com/youtube/v3/search/",{
         part: "snippet",
         key: "AIzaSyDPZyHPbSzP13beJ-81xwrfzo-W8Sl1I_Q",
-        maxResults: 10,
+        maxResults: 8,
         type:'video',
         q: e
     }, function(data) {
+       var nextPageToken = data.nextPageToken;
+    var prevPageToken = data.prevPageToken;
+    var prevnext = buildButton(nextPageToken,prevPageToken);
+    $('#nav-buttons').append(prevnext);
+
     //iterate on the acquired number of json objects and append to output    
     $.each(data.items, function(i, item) {
-            var individual_result = jsonResults(item);
-            $('#search-results').append(individual_result);
+        var individual_result = jsonResults(item);
+        $('#search-results').append(individual_result);
         });
         //test the response recieved with console.log
         console.log(data.items);
     })
+    
+ 
 }
 
 //single result format
@@ -41,7 +48,17 @@ function jsonResults(e) {
             desc = e.snippet.description,
             thumbnail = e.snippet.thumbnails.medium.url,
             published = e.snippet.publishedAt;  
-            var single_item='<li id="result-card">'+'<img id="img" class="col-md-4" style="border-radius: 15px;" src="'+thumbnail+'">'+'<div class="col-md-8">'+'<ul id="item-desc" style="list-style: none;">'+'<li id="title">'+title+'</li>'+'<li>'+desc+'</li>'+'<li>'+published+'</li>'+'</ul>'+'</div>'+'</li>';
+            channelTitle = e.snippet.channelTitle;
+            var single_item='<li id="result-card">'+'<a class="afterglow" href="#vid">'+'<img id="img" class="col-md-4" style="border-radius: 15px;" src="'+thumbnail+'">'+'<div class="col-md-8">'+'<ul id="item-desc" style="list-style: none;">'+'<li id="title">'+title+'</li>'+'<li>'+channelTitle+'</li>'+'<li>'+desc+'</li>'+'<li>'+published+'</li>'+'</ul>'+'</div>'+'</a>'+'</li>';
             return single_item;
 }
-
+    
+function buildButton(nextPageToken,prevPageToken){
+    if(!prevPageToken)
+        var validButtons = '<div>'+'<img align="right" width="50px" height="50px" src="img/next.png" onClick>'+'</div>';
+    else if(!nextPageToken)
+        var validButtons = '<div>'+'<img align="left" width="50px" height="50px" src="img/previous.png">'+'</div>';
+    else
+        var validButtons = '<div>'+'<img align="left" width="50px" height="50px" src="img/previous.png">'+'<img align="right" width="50px" height="50px" src="img/next.png">'+'</div>';
+    return validButtons;
+}
