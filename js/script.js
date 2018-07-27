1fe521a699c25fb0990215a487aa9868497e4ce6
alias-3 <wanianurag3@gmail.com>
2018-07-27 14:34:36 +0530

    added functional nav buttons

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ $(function() {
         //take query from user input
         var t = $("#squery").val();
         //clear the previous console log taken for testing
-        console. clear();
         search(t);
 
     })
@@ -21,7 +20,47 @@ function search(e) {
         key: "AIzaSyDPZyHPbSzP13beJ-81xwrfzo-W8Sl1I_Q",
         maxResults: 8,
         type:'video',
+        regionCode : 'IN',
         q: e
+    }, function(data) {
+    var nextPageToken = data.nextPageToken;
+    var prevPageToken = data.prevPageToken;
+    var prevnext = buildButton(nextPageToken,prevPageToken);
+    $('#nav-buttons').append(prevnext);
+
+    //iterate on the acquired number of json objects and append to output    
+    $.each(data.items, function(i, item) {
+        var individual_result = jsonResults(item);
+        $('#search-results').append(individual_result);
+        });
+        //test the response recieved with console.log
+        console.clear();
+        console.log(data.items);
+    })
+    
+ 
+}
+
+
+function searchnav(e) {
+   
+    if (e)
+        var token = $('#navnext').data('token');
+    else
+        var token = $('#navprev').data('token');    
+
+    $('#search-results').html(' ');
+    $('#nav-buttons').html(' ');  
+    q = $('#squery').val();
+    //brief get request made to the youtube data api
+    $.getJSON("https://www.googleapis.com/youtube/v3/search/",{
+        part: "snippet",
+        key: "AIzaSyDPZyHPbSzP13beJ-81xwrfzo-W8Sl1I_Q",
+        maxResults: 8,
+        pageToken : token,
+        type:'video',
+        regionCode:'in',
+        q: q
     }, function(data) {
        var nextPageToken = data.nextPageToken;
     var prevPageToken = data.prevPageToken;
@@ -34,6 +73,7 @@ function search(e) {
         $('#search-results').append(individual_result);
         });
         //test the response recieved with console.log
+        console.clear();
         console.log(data.items);
     })
     
@@ -49,16 +89,16 @@ function jsonResults(e) {
             thumbnail = e.snippet.thumbnails.medium.url,
             published = e.snippet.publishedAt;  
             channelTitle = e.snippet.channelTitle;
-            var single_item='<li id="result-card">'+'<a class="afterglow" href="#vid">'+'<img id="img" class="col-md-4" style="border-radius: 15px;" src="'+thumbnail+'">'+'<div class="col-md-8">'+'<ul id="item-desc" style="list-style: none;">'+'<li id="title">'+title+'</li>'+'<li>'+channelTitle+'</li>'+'<li>'+desc+'</li>'+'<li>'+published+'</li>'+'</ul>'+'</div>'+'</a>'+'</li>';
+            var single_item='<li id="result-card"><a href="http://www.youtube.com/embed/'+vidId+'"><img id="img" class="col-md-4" src="'+thumbnail+'"><div class="col-md-8"><ul id="item-desc" style="list-style: none;"><li id="title">'+title+'</li><li>By '+channelTitle+'</li><li>'+desc+'</li><li>'+published+'</li></ul></div></a></li>';
             return single_item;
 }
     
 function buildButton(nextPageToken,prevPageToken){
     if(!prevPageToken)
-        var validButtons = '<div>'+'<img align="right" width="50px" height="50px" src="img/next.png" onClick>'+'</div>';
+        var validButtons = '<div><img id="navnext" align="right" data-token="'+nextPageToken+'"  onClick="searchnav(1)" width="50px" height="50px" src="img/for.png"></div>';
     else if(!nextPageToken)
-        var validButtons = '<div>'+'<img align="left" width="50px" height="50px" src="img/previous.png">'+'</div>';
+        var validButtons = '<div><img id="navprev" align="left" data-token="'+prevPageToken+'" onClick="searchnav()" width="50px" height="50px" src="img/back.png"></div>';
     else
-        var validButtons = '<div>'+'<img align="left" width="50px" height="50px" src="img/previous.png">'+'<img align="right" width="50px" height="50px" src="img/next.png">'+'</div>';
+        var validButtons = '<div><img id="navprev" align="left" data-token="'+prevPageToken+'" onClick="searchnav()" width="50px" height="50px" src="img/back.png"><a href="Tube Finder!.html"><img align="center" src="img/ref.png" width="50px" height="50px"><a><img id="navnext" align="right" data-token="'+nextPageToken+'" onClick="searchnav(1)" width="50px" height="50px" src="img/for.png"></div>';
     return validButtons;
 }
